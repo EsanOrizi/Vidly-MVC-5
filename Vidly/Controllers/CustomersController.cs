4479b4b8f78c3f269bd5c29959ce582abf58e59a
Esan <Esan@DESKTOP-T06NDV6>
2020-03-27 23:59:44 +0000

    3-8 Querying Objects from the database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -7,16 +7,41 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+
+        
+        // 1. need a db context to access the database
+        private ApplicationDbContext _context;
+
+        // 2. initialize the _context
+        public CustomersController()
+        {
+
+            _context = new ApplicationDbContext();
+
+        }
+
+        // 3. db context is a disposable object so we need to properly dispose of it
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            // 4. get the customer list from the database 
+            // 5. when this line is executed, entity framework is not going to query the database, deferred execution
+            // the query is excuted when we iterate over the object
+            // .ToList after the customer executes the query staright away
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            // 6. get the data from the database, becuase of SingleOrDefault our query is executed straight away
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -24,6 +49,11 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
+
+
+
+        // 0. get rid of this method and get the list of customers from the database
+        /*
         private IEnumerable<Customer> GetCustomers()
         {
             return new List<Customer>
@@ -32,5 +62,6 @@ namespace Vidly.Controllers
                 new Customer { Id = 2, Name = "Mary Williams" }
             };
         }
+        */
     }
 }
