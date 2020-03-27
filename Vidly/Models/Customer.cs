bd5fd10f4d75f1ad51bf7089d4ea99435aa5e6c8
Esan <Esan@DESKTOP-T06NDV6>
2020-03-27 21:50:52 +0000

    3-5 Changing the Model - MembershipType model added

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,11 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        // To add IsSubscribedToNewsLetter to customer class and customer table 
+        public bool IsSubscribedToNewsLetter { get; set; }
+        // navigation property, allows us to navigate from customer to membership type and load both from the database
+        public MembershipType MembershipType { get; set; }
+        // this property is a foreign key 
+        public byte MembershipTypeId { get; set; }
     }
 }
